00441ac5df6b3b3bc4e5e1fe67b8be52164d117b
杨晓伟 <yxw610@126.com>
2016-01-14 11:34:47 +0800

    发生的

--- TestForGitHub/TestForGitHub/AppDelegate.m
+++ TestForGitHub/TestForGitHub/AppDelegate.m
@@ -10,7 +10,7 @@
 
 @interface AppDelegate ()
 
-@property (strong, nonatomic) NSMutableArray * array;
+@property (strong, nonatomic) NSMutableArray * array3;
 @property (strong, nonatomic) UIButton * button;
 @property (strong, nonatomic) UITextField *textField;
 
